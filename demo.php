1d89422033223e8c6d65b7681a23f09576f170e9
zhanguangcheng <14712905@qq.com>
2018-01-31 16:07:41 +0800

    添加 2017-11 的数据

--- demo.php
+++ demo.php
@@ -7,6 +7,7 @@ $data = array(
     array('date'=>'2015-12', 'url'=>'http://www.mca.gov.cn/article/sj/tjbz/a/2015/201706011127.html'),
     array('date'=>'2016-12', 'url'=>'http://www.mca.gov.cn/article/sj/tjbz/a/2016/201612/201705311652.html'),
     array('date'=>'2017-08', 'url'=>'http://www.mca.gov.cn/article/sj/tjbz/a/2017/20178/201709251028.html'),
+    array('date'=>'2017-11', 'url'=>'http://www.mca.gov.cn/article/sj/tjbz/a/2017/201801/201801151447.html'),
 );
 
 foreach ($data as $key => $value) {
